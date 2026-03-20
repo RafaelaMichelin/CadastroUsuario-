ab252aa8497acb741ee521a8cda24e28babab8cf
RafaelaMichelin <RafaelaMichelin@gmail.com>
2026-03-19 21:56:57 -0300

    Implementando método para receber dados de nome e email e click para salvar

--- app/src/main/java/com/rafaela/cadastrousurio/CreateUser.java
+++ app/src/main/java/com/rafaela/cadastrousurio/CreateUser.java
@@ -1,5 +1,6 @@
 package com.rafaela.cadastrousurio;
 
+import android.os.Bundle;
 import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 import com.google.android.material.textfield.TextInputEditText;
@@ -10,4 +11,29 @@ public class CreateUser extends AppCompatActivity {
 
     Button btnSalvar;
 
+    //notação para sobreescrever metodo
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.create_user);
+
+        //Vincula variável com a interface gráfica, escrito no XML
+        editNome = findViewById(R.id.nome);
+        editEmail = findViewById(R.id.email);
+        btnSalvar = findViewById(R.id.btnSalvar);
+
+        //função de click
+        btnSalvar.setOnClickListener(v -> {
+            //pega oque for digitado no input nome e email, converte para string
+            String nome = editNome.getText().toString();
+            String email = editEmail.getText().toString();
+
+            if(!nome.isEmpty())
+            {
+
+                MainActivity.listaNomes.add(nome + "(" + email +")");
+                finish();
+            }
+        });
+    }
 }
