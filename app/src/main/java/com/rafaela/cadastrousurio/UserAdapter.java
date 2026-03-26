4504cc098fd0412f1bd1115fbd61fa77e65fcc81
RafaelaMichelin <RafaelaMichelin@gmail.com>
2026-03-26 19:44:21 -0300

    Implementando UserAdapter para gerenciar a criação dos itens

--- app/src/main/java/com/rafaela/cadastrousurio/UserAdapter.java
+++ app/src/main/java/com/rafaela/cadastrousurio/UserAdapter.java
@@ -1,5 +1,37 @@
 package com.rafaela.cadastrousurio;
 
-public class UserAdapter {
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.List;
+
+//Classe Adapter: Gerencia a criação e o preenchimento dos itens na RecycleView
+public class UserAdapter extends RecyclerView.Adapter<UserAdapter.ViewHolder> {
+
+    //Atributo privado que armazena a referência dos dados que serão exibidos
+    private List<String> listaUsuarios;
+
+    //Construtor que permite que a MainActivity "entregue" a lista de dados para este Adapter
+   public UserAdapter(List<String> lista){
+       this.listaUsuarios = lista;
+
+   }
+
+   //Método: Cria do zero o visual de uma linha da lista(+ViewHolder)
+
+    @NonNull
+    @Override   //sobreescrever método
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType){
+       //LayoutInflater transforma o arquivo XML em um objeto VIEW JAVA
+
+        //Aqui usamos um Layout padrão do Android (simple_list_item_1) para facilitar
+        View view = LayoutInflater.from(parent.getContext()).inflate(android.R.layout.simple_list_item_1,parent,false);
+
+    }
 }
